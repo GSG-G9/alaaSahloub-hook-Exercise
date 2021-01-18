5804c7992a4920f7a86004d510b972b5eb648c65
Alaa Sahloub <alaa.sahloub.eb@gmail.com>
2021-01-18 14:55:58 +0200

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Component/Exercise1'
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
+    <h1>React hook useState</h1>
+    <Counter/>
     </>
   );
 }
